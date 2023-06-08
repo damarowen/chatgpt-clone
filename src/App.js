b5fe03e7e89393683c3f81db98d92b5cebd84f46
damarowen <damarowen@gmailcom>
2023-06-08 22:24:11 +0700

    coding chatbot interfac

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./normal.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className="sidemenu">
+        <div className="sidemenu-button">
+          <span>+</span>
+          New Chat
+        </div>
+      </aside>
+      <section className="chatbox">
+        <div className="chat-log">
+          <div className="chat-message">
+            <div className="avatar">Me</div>
+            <div className="message">Message</div>
+          </div>
+        </div>
+        <div className="chat-input-holder">
+          <textarea
+            className="chat-input-textarea"
+            placeholder="Type your message here"
+          ></textarea>
+        </div>
+      </section>
     </div>
   );
 }
