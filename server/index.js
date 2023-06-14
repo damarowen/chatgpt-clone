d4cab29a7557d68625922d94331e2945f2e2be9b
damarowen <damarowen@gmailcom>
2023-06-14 16:47:47 +0700

    add alert,revamp ui

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 require("dotenv").config();
 
 const configuration = new Configuration({
-  organization: "org-Ratuv0X5bHbIHK38XXp2nN0e",
   apiKey: process.env.OPENAI_API_KEY,
 });
 
@@ -19,39 +18,66 @@ const port = 3010;
 
 const instruction = {
   role: "system",
-  content: "You are ChatGPT, a large language model trained by OpenAI. Carefully heed the user's instructions. Respond using Markdown.",
+  content:
+    "You are ChatGPT, a large language model trained by OpenAI. Carefully heed the user's instructions. Respond using Markdown.",
 };
 
 let chatHistory = [instruction]; // Store the chat history
 
 app.post("/", async (req, res) => {
-  let { message, currentModel } = req.body;
-  if (!currentModel) currentModel = "gpt-3.5-turbo";
+  try {
 
-  // Add the user's message to the chat history
-  chatHistory.push({ role: "user", content: message });
+    let { message, currentModel } = req.body;
 
-  const response = await openai.createChatCompletion({
-    model: `${currentModel}`,
-    messages: chatHistory,
-    max_tokens: 100,
-    temperature: 0.5,
-  });
+    if (!currentModel) currentModel = "gpt-3.5-turbo";
 
-  // Add the assistant's response to the chat history
-  chatHistory.push(response.data.choices[0].message);
+    // Add the user's message to the chat history
+    chatHistory.push({ role: "user", content: message });
 
-  console.log(chatHistory, "<<<<<<<<<<<<<chatHistory<<<<<");
-  res.json({
-    message: response.data.choices[0].message,
-  });
+    const response = await openai.createChatCompletion({
+      model: `${currentModel}`,
+      messages: chatHistory,
+      max_tokens: 100,
+      temperature: 0.5,
+    });
+
+    // Add the assistant's response to the chat history
+    chatHistory.push(response.data.choices[0].message);
+
+    res.json({
+      message: response.data.choices[0].message,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: `An error occurred ${error}.` });
+  }
 });
 
 app.get("/models", async (req, res) => {
-  const response = await openai.listEngines();
-  res.json({
-    models: response.data,
-  });
+  try {
+    const response = await openai.listModels();
+    const filteredModels = response.data.data.filter(model => model.id.includes("gpt"));
+
+    res.json({
+      data: filteredModels,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: `An error occurred ${error}.` });
+  }
+});
+
+app.get("/clear", async (req, res) => {
+  try {
+    chatHistory = [instruction];
+    res.json({
+      message: "success",
+      chatHistory,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: `An error occurred ${error}.` });
+  }
 });
 
 app.listen(port, () => {
